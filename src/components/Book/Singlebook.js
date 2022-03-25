120692dc7cc2a527fabb1f7118f0ce3a84d7c0ea
unknown <islam15-3879@diu.edu.bd>
2022-03-26 00:04:41 +0600

    font-added

--- src/components/Book/Singlebook.js
+++ src/components/Book/Singlebook.js
@@ -1,16 +1,20 @@
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import './Singlebook.css'
 
+
 const Singlebook = (props) => {
     console.log(props)
+    const{image,name,price}=props.book;
     return (
         <div className='book' >
             
-            <img src={props.book.image} alt="" />
-            <p> Name: {props.book.name}</p>
-            <p> Price:$ {props.book.price}</p>
+            <img src={image} alt="" />
+            <p> Name: {name}</p>
+            <p> Price:$ {price}</p>
             <p>{props.book.id}</p>
-            <button>Add To Cart</button>
+            <button>Add To Cart   <FontAwesomeIcon className='font-icon' icon={faShoppingCart}></FontAwesomeIcon> </button>
             
             
         </div>
